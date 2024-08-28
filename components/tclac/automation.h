a807079cce05e98997669aba23006ec72cdd9e24
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2024-08-28 20:05:13 +0400

    changes

--- components/tclac/automation.h
+++ components/tclac/automation.h
@@ -28,59 +28,59 @@ class DisplaySwitch : public BaseSwitch {
 };
 #endif
 
-template<typename... Ts> class TclActionBase : public Action<Ts...>, public Parented<TclClimate> {};
+template<typename... Ts> class BaseAction : public Action<Ts...>, public Parented<TclClimate> {};
 
 // Шаблон действия: изменение вертикальной фиксации заслонки
-template<typename... Ts> class VerticalAirflowAction : public TclActionBase<Ts...> {
+template<typename... Ts> class VerticalAirflowAction : public BaseAction<Ts...> {
   TEMPLATABLE_VALUE(AirflowVerticalDirection, direction)
   void play(Ts... x) { this->parent_->set_vertical_airflow(this->direction_.value(x...)); }
 };
 
 // Шаблон действия: изменение горизонтальной фиксации заслонок
-template<typename... Ts> class HorizontalAirflowAction : public TclActionBase<Ts...> {
+template<typename... Ts> class HorizontalAirflowAction : public BaseAction<Ts...> {
   TEMPLATABLE_VALUE(AirflowHorizontalDirection, direction)
   void play(Ts... x) { this->parent_->set_horizontal_airflow(this->direction_.value(x...)); }
 };
 
 // Шаблон действия: изменение режима вертикального качания заслонки
-template<typename... Ts> class VerticalSwingDirectionAction : public TclActionBase<Ts...> {
+template<typename... Ts> class VerticalSwingDirectionAction : public BaseAction<Ts...> {
   TEMPLATABLE_VALUE(VerticalSwingDirection, direction)
   void play(Ts... x) { this->parent_->set_vertical_swing_direction(this->direction_.value(x...)); }
 };
 
 // Шаблон действия: изменение режима горизонтального качания заслонок
-template<typename... Ts> class HorizontalSwingDirectionAction : public TclActionBase<Ts...> {
+template<typename... Ts> class HorizontalSwingDirectionAction : public BaseAction<Ts...> {
   TEMPLATABLE_VALUE(HorizontalSwingDirection, direction)
   void play(Ts... x) { this->parent_->set_horizontal_swing_direction(this->direction_.value(x...)); }
 };
 
 // Шаблон действия: включение дисплея
-template<typename... Ts> class DisplayOnAction : public TclActionBase<Ts...> {
+template<typename... Ts> class DisplayOnAction : public BaseAction<Ts...> {
   void play(Ts... x) { this->parent_->set_display_state(true); }
 };
 
 // Шаблон действия: выключение дисплея
-template<typename... Ts> class DisplayOffAction : public TclActionBase<Ts...> {
+template<typename... Ts> class DisplayOffAction : public BaseAction<Ts...> {
   void play(Ts... x) { this->parent_->set_display_state(false); }
 };
 
 // Шаблон действия: включение пищалки
-template<typename... Ts> class BeeperOnAction : public TclActionBase<Ts...> {
+template<typename... Ts> class BeeperOnAction : public BaseAction<Ts...> {
   void play(Ts... x) { this->parent_->set_beeper_state(true); }
 };
 
 // Шаблон действия: выклюение пищалки
-template<typename... Ts> class BeeperOffAction : public TclActionBase<Ts...> {
+template<typename... Ts> class BeeperOffAction : public BaseAction<Ts...> {
   void play(Ts... x) { this->parent_->set_beeper_state(false); }
 };
 
 // Шаблон действия: включение принудительного применения настроек
-template<typename... Ts> class ForceOnAction : public TclActionBase<Ts...> {
+template<typename... Ts> class ForceOnAction : public BaseAction<Ts...> {
   void play(Ts... x) { this->parent_->set_force_mode_state(true); }
 };
 
 // Шаблон действия: выключение принудительного применения настроек
-template<typename... Ts> class ForceOffAction : public TclActionBase<Ts...> {
+template<typename... Ts> class ForceOffAction : public BaseAction<Ts...> {
   void play(Ts... x) { this->parent_->set_force_mode_state(false); }
 };
 
