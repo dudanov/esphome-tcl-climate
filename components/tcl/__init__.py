cbd991842ceccc7fd5dd18efa23b41e369b4ae6e
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2024-08-29 15:40:28 +0400

    changes

--- components/tcl/__init__.py
+++ components/tcl/__init__.py
@@ -3,7 +3,6 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.cpp_helpers import register_parented
 
-AUTO_LOAD = ["climate", "switch"]
 DOMAIN = "tcl"
 CONF_TCL_ID = "tcl_id"
 CONF_FORCE = "force"
