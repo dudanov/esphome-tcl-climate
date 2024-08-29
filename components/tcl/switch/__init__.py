79f46bfc874918db7c2fe2f16326494be05f74ec
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2024-08-29 16:05:47 +0400

    test

--- components/tcl/switch/__init__.py
+++ components/tcl/switch/__init__.py
@@ -1,7 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components.switch import Switch, new_switch, switch_schema
-from esphome.const import CONF_BEEPER, CONF_DISPLAY, ENTITY_CATEGORY_CONFIG
+from esphome.const import CONF_BEEPER, CONF_DISPLAY, ENTITY_CATEGORY_CONFIG, CONF_NAME
+from esphome import automation
 
 from .. import CONF_TCL_ID, TclBase, register_tcl, tcl_ns, CONF_FORCE
 
@@ -12,25 +13,22 @@ DisplaySwitch = tcl_ns.class_("DisplaySwitch", BaseSwitch)
 ForceSwitch = tcl_ns.class_("ForceSwitch", BaseSwitch)
 
 
+def tcl_simple(_class):
+    return automation.maybe_conf(
+        CONF_NAME,
+        switch_schema(
+            _class,
+            entity_category=ENTITY_CATEGORY_CONFIG,
+            icon="mdi:volume-source",
+            default_restore_mode="RESTORE_DEFAULT_ON",
+        ),
+    )
+
+
 SWITCH_TYPES = {
-    CONF_BEEPER: switch_schema(
-        BeeperSwitch,
-        entity_category=ENTITY_CATEGORY_CONFIG,
-        icon="mdi:volume-source",
-        default_restore_mode="RESTORE_DEFAULT_ON",
-    ),
-    CONF_DISPLAY: switch_schema(
-        DisplaySwitch,
-        entity_category=ENTITY_CATEGORY_CONFIG,
-        icon="mdi:volume-source",
-        default_restore_mode="RESTORE_DEFAULT_ON",
-    ),
-    CONF_FORCE: switch_schema(
-        ForceSwitch,
-        entity_category=ENTITY_CATEGORY_CONFIG,
-        icon="mdi:volume-source",
-        default_restore_mode="RESTORE_DEFAULT_ON",
-    ),
+    CONF_BEEPER: tcl_simple(BeeperSwitch),
+    CONF_DISPLAY: tcl_simple(DisplaySwitch),
+    CONF_FORCE: tcl_simple(ForceSwitch),
 }
 
 CONFIG_SCHEMA = cv.Schema(
